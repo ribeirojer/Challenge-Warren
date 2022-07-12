fee13eac9b93d9712daca2f51893bd8a72517fe9
José Eduardo Ribeiro <100392655+ribeirojer@users.noreply.github.com>
2022-07-12 17:29:05 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,10 @@ type Props = {}
 const Header = (props: Props) => {
   return (
     <header data-testid="header" className={styles.header}>
-        <Link to={"/"}><img src=".\src\assets\warren-black-logo.svg" alt="logo-warren" /></Link>
+        <Link to={"/"}><img src={".\src\assets\warren-black-logo.svg"} alt="logo-warren" /></Link>
         <div className={styles.header__user}>
             <span className={styles.header__user__name}>Buffett</span>
-            <img className={styles.header__user__photo} src=".\src\assets\joe-hepburn-qr7rfIthbvc-unsplash.jpg" alt="foto-user" />
+            <img className={styles.header__user__photo} src={".\src\assets\joe-hepburn-qr7rfIthbvc-unsplash.jpg"} alt="foto-user" />
         </div>
     </header>
   )
